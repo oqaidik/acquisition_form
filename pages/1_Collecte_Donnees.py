3041ca797157ba19fc61bcf2227e6e39e474c46f
Khalid Oqaidi <70282956+oqaidik@users.noreply.github.com>
2025-04-13 08:16:42 +0100

    Update 1_Collecte_Donnees.py

--- pages/1_Collecte_Donnees.py
+++ pages/1_Collecte_Donnees.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pygsheets
+import json
+
+st.title("Page : Collecte des Données")
+
+creds = st.secrets["GOOGLE_SHEETS_CREDS"]
+gc = pygsheets.authorize(service_account_info=json.loads(creds))
+# sh = gc.open('NomDeVotreFichierGoogleSheets')
+sh = gc.open('str1')
+wks = sh[0]
+
+nom = st.text_input("Nom")
+email = st.text_input("Email")
+
+if st.button("Ajouter"):
+    if nom and email:
+        wks.append_table([nom, email])
+        st.success("Les données ont été ajoutées avec succès !")
+    else:
+        st.warning("Veuillez remplir tous les champs.")
 
