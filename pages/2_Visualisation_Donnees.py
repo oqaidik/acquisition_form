f35a84b81f36e8e8cfeaf8306511fc9239cd371a
Khalid Oqaidi <70282956+oqaidik@users.noreply.github.com>
2025-04-13 08:17:39 +0100

    Update 2_Visualisation_Donnees.py

--- pages/2_Visualisation_Donnees.py
+++ pages/2_Visualisation_Donnees.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pygsheets
+import json
+import pandas as pd
+
+st.title("Page : Visualisation des Données")
+
+creds = st.secrets["GOOGLE_SHEETS_CREDS"]
+gc = pygsheets.authorize(service_account_info=json.loads(creds))
+sh = gc.open('str1')
+wks = sh[0]
+
+df = wks.get_as_df()
+
+st.subheader("Données enregistrées")
+st.dataframe(df)
 
